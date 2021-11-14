fa1c57aa4350e0be3c5924af960911b59e9895dd
Aunali321 <aunvakil.aa@gmail.com>
2021-11-14 17:17:01 +0530

    improved website design and carousel now works.

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 const URL1 = "https://jsonplaceholder.typicode.com/todos/1";
 
 const newsContainerElement = document.getElementById('newsContainer');
-var getNewsElement = document.getElementById("getbutton");
+const carousel0Element = document.getElementById('carousel0');
+const carousel1Element = document.getElementById('carousel1');
+const carousel2Element = document.getElementById('carousel2');
+const carousel3Element = document.getElementById('carousel3');
 
-getNewsElement.addEventListener("click", async function() {
-await getNews();
-});
+// var getNewsElement = document.getElementById("getbutton");
+
+
+// getNewsElement.addEventListener("click", async function() {
+// await getNews();
+// });
 
 async function getNews() {
   await fetch('http://localhost:3000', {
@@ -22,26 +28,54 @@ async function getNews() {
     res.json().then((data) => {
       let html = '';
       if(data.articles) {
+        var carousel0Data = data.articles[40];
+        var carousel1Data = data.articles[41];
+        var carousel2Data = data.articles[42];
+        var carousel3Data = data.articles[43];
+
+        carousel0Element.innerHTML = `
+        <div class="overlay-image" style="background-image: url(${carousel0Data.urlToImage});"></div>
+        <a href="${carousel0Data.url}" id="carouselHeadline">${carousel0Data.title}</a>
+        <p id="carousel-item">${carousel0Data.description}</p>
+        `;
+        carousel1Element.innerHTML = `
+        <div class="overlay-image" style="background-image: url(${carousel1Data.urlToImage});"></div>
+        <a href="${carousel0Data.url}" id="carouselHeadline">${carousel1Data.title}</a>
+        <p id="carousel-item">${carousel1Data.description}</p>
+        `;
+        carousel2Element.innerHTML = `
+        <div class="overlay-image" style="background-image: url(${carousel2Data.urlToImage});"></div>
+        <a href="${carousel0Data.url}" id="carouselHeadline">${carousel2Data.title}</a>
+        <p id="carousel-item">${carousel2Data.description}</p>
+        `;
+        carousel3Element.innerHTML = `
+        <div class="overlay-image" style="background-image: url(${carousel3Data.urlToImage});"></div>
+        <a href="${carousel0Data.url}" id="carouselHeadline">${carousel3Data.title}</a>
+        <p id="carousel-item">${carousel3Data.description}</p>
+        `;
+
         data.articles.forEach(article => {
-          console.log(article);
-          html += `
-          <div class="col-md">
-            <div class="card">
+          html += ` 
+          <div class="col-sm-12 col-md-6 col-lg-4 py-3" id="col">
+            <div class="card" id="newsCard">
               <div class="card-body">
-                <h5 class="card-title">${article.title}</h5>
-                <img src="${article.urlToImage}" alt="" class="card-img-top" id="overlay-img-news">
+                <a href="${article.url}" class="card-title" id="link">${article.title}</a>
+                <img src="${article.urlToImage}" alt="" class="card-img-top p-3" id="overlay-img-news">
                 <p class="card-text">
                   ${article.description}
                 </p>
-                <a href="${article.link}" class="btn btn-primary">Go to news</a>
               </div>
             </div>
           </div>
           `
         });       
+      } else {
+        html = "Sorry, We didn't find any news"
       }
 
       newsContainerElement.innerHTML = html;
         });
   });
 }
+
+getNews();
